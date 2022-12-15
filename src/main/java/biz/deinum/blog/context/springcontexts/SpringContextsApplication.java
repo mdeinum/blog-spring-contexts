cafdbfd76507f7aace51df374ef4f062364dcdd8
Marten Deinum <mdeinum@gmail.com>
2022-12-15 14:09:53 +0100

    Upgrade for Java 17 and Spring Framework 6

--- src/main/java/biz/deinum/blog/context/springcontexts/SpringContextsApplication.java
+++ src/main/java/biz/deinum/blog/context/springcontexts/SpringContextsApplication.java
@@ -1,6 +1,5 @@
 package biz.deinum.blog.context.springcontexts;
 
-import org.springframework.beans.factory.support.BeanDefinitionReader;
 import org.springframework.beans.factory.support.PropertiesBeanDefinitionReader;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.context.ApplicationContext;
@@ -10,40 +9,46 @@ import org.springframework.context.support.GenericApplicationContext;
 
 public class SpringContextsApplication {
 
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
-		// Properties
-		GenericApplicationContext contextFromProperties = new GenericApplicationContext();
-		BeanDefinitionReader propertiesReader = new PropertiesBeanDefinitionReader(contextFromProperties);
-		propertiesReader.loadBeanDefinitions("classpath:application-context.properties");
-		contextFromProperties.refresh();
-		doGreeting(contextFromProperties);
-		contextFromProperties.stop();
+        // Properties
+        try (var contextFromProperties = new GenericApplicationContext()) {
+            var propertiesReader = new PropertiesBeanDefinitionReader(contextFromProperties);
+            propertiesReader.loadBeanDefinitions("classpath:application-context.properties");
+            contextFromProperties.refresh();
+            doGreeting(contextFromProperties);
+        }
 
-		// XML
-		GenericApplicationContext contextFromXml = new GenericApplicationContext();
-		BeanDefinitionReader xmlReader = new XmlBeanDefinitionReader(contextFromXml);
-		xmlReader.loadBeanDefinitions("classpath:applicationContext.xml");
-		contextFromXml.refresh();
-		doGreeting(contextFromXml);
-		contextFromXml.stop();
+        // XML
+        try (var contextFromXml = new GenericApplicationContext()) {
+            var xmlReader = new XmlBeanDefinitionReader(contextFromXml);
+            xmlReader.loadBeanDefinitions("classpath:applicationContext.xml");
+            contextFromXml.refresh();
+            doGreeting(contextFromXml);
+        }
 
-		// The same as above but now the loading is encapsulated
-		ApplicationContext xmlContext = new ClassPathXmlApplicationContext("applicationContext.xml");
-		doGreeting(xmlContext);
-		((ClassPathXmlApplicationContext) xmlContext).stop();
+        // The same as above but now the loading is encapsulated
+        try (var xmlContext = new ClassPathXmlApplicationContext("applicationContext.xml")) {
+            doGreeting(xmlContext);
+        }
 
-		// Java Configuration
-		ApplicationContext javaContext = new AnnotationConfigApplicationContext(ApplicationConfig.class);
-		doGreeting(xmlContext);
-		((AnnotationConfigApplicationContext) javaContext).close();
+        // Java Configuration
+        try (var javaContext = new AnnotationConfigApplicationContext(ApplicationConfig.class)) {
+            doGreeting(javaContext);
+        }
 
-	}
+        // Java Configuration with Functional Bean Registation
+        try (var functionalContext = new GenericApplicationContext()) {
+            functionalContext.registerBean("person", Person.class, () -> new Person("Marten Deinum"));
+			functionalContext.registerBean("greeter", Greeter.class, SystemOutGreeter::new);
+			functionalContext.refresh();
+			doGreeting(functionalContext);
+        }
+    }
 
-	private static void doGreeting(ApplicationContext ctx) {
-		Greeter greeter = ctx.getBean(Greeter.class);
-		Person person = ctx.getBean(Person.class);
-		greeter.greet(person);
-
-	}
+    private static void doGreeting(ApplicationContext ctx) {
+        var greeter = ctx.getBean(Greeter.class);
+        var person = ctx.getBean(Person.class);
+        greeter.greet(person);
+    }
 }
